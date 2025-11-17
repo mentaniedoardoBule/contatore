6b9dd90040647a5c99f90646746c1262f639c8ef
mentaniedoardoBule <mentaniedoardo@gmail.com>
2025-11-17 12:26:22 +0100

    amo gli unicorni

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,38 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        CounterSynchronized c1 = new CounterSynchronized();
+        Counter c2 = new Counter();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        // Create multiple threads to increment the counter
+        Thread t1 = new Thread(() -> c1.increment());
+        Thread t2 = new Thread(() -> c1.increment());
+        Thread t3 = new Thread(() -> c2.increment());
+        Thread t4 = new Thread(() -> c2.increment());
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        t3.start();
+        t4.start();
+
+        try {
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final count: " + c1.getCount());
+        System.out.println("Final count: " + c2.getCount());
+
     }
 }
