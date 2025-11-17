eb605aee06e00318a8096af356b447e975d41aad
mentaniedoardoBule <mentaniedoardo@gmail.com>
2025-11-17 12:39:30 +0100

    amo gli unicorni

---
+++ src/CounterSynchronized.java
@@ -0,0 +1,17 @@
+public class CounterSynchronized {
+        private final Object lock = new Object();
+        private int count = 0;
+
+        public void increment() {
+            synchronized (lock) {
+                count++;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+    }
+
+
